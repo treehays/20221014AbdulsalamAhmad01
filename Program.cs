698ff2e5f25a1523cfc640d1cf0018f85d6a3758
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-14 11:47:22 +0100

    alll

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Question
+{
+    class Program
+    {
+        static int result(int num2, int num1)
+        {
+            int count = 0;
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 >= num2)
+                {
+                    num1 -= num2;
+                }
+                else
+                {
+                    num2 -= num1;
+                }
+                ++count;
+            }
+            return count;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(result(num1, num2));
+        }
+
+    }
+
+}
