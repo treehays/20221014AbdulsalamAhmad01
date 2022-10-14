be872f399b6fd10ccc234a23c77a4a8f00d58a1e
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-14 20:49:56 +0100

    re

--- Question01/Program.cs
+++ Question01/Program.cs
@@ -1,19 +1,40 @@
-string title = "hgFirst Letter of EFDach word";
-
-string[] arr = title.Split(" ");
-// string temp = "";
-
-for (int i = 0; i < arr.Length; i++)
+namespace Question
 {
-    if (arr[i].Length <= 2)
+    class Program
     {
-        Console.Write(arr[i].ToLower());
+        static void result(String title)
+        {
+
+            string[] arr = title.Split(" ");
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length <= 2)
+                {
+                    Console.Write(arr[i].ToLower());
+                }
+                else
+                {
+                    Console.Write((arr[i].ToUpper())[0]);
+                    Console.Write(arr[i].ToLower().Remove(0, 1));
+                }
+                Console.Write(" ");
+
+            }
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Sentence: ");
+            String title = Console.ReadLine();
+            
+
+            result(title);
+        }
+
     }
-    else
-    {
-        Console.Write((arr[i].ToUpper())[0]);
-        Console.Write(arr[i].ToLower().Remove(0, 1));
-    }
-    Console.Write(" ");
 
 }
